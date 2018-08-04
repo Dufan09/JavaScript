0aa82580f78940f9db49f9b9ec4f4d02f1bd7e52
Dufan09 <41882784+Dufan09@users.noreply.github.com>
2018-08-04 18:52:35 +0800

    ship2

--- battleship.js
+++ battleship.js
@@ -1,25 +1,31 @@
-var location1=3;
-var location2=4;
-var location3=5;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk= false;
 
-while (isSunk==false){
-    guess=prompt("Ready,aim,fire!(enter a number 0-6):");
-        if (guess<0||guess>6) {alert("Please enter a valid cell number!");}
-        else {guesses=guesses+1;
-            if (guess==location1||guess==location2||guess==location3) 
-            {
-                alert("HIT!");
-                hits=hits+1;
-                if (hits==3) {
-                	isSunk=true;
-                	alert("You sank my battleship!");
+class Solution {
+    public static String countAndSay(int n) {
+        if(n==1) {
+            return "1";
+        }
+        String fir = "11";
+        char[] ch = fir.toCharArray();
+        int count = 1;
+        int n1 = 0;
+            while (n > 2) {
+                StringBuffer sb = new StringBuffer();
+                for (int i = 1; i < ch.length; i++) {
+                    n1 = ch[i];
+                    if (n1 == ch[i - 1]) {
+                        count++;
+                    } else {
+                        sb.append(count).append(ch[i - 1]);
+                        count = 1;
+                    }
+                    if (i == ch.length - 1) {
+                        sb.append(count).append(ch[i]);
+                    }
                 }
+                count=1;
+                ch=sb.toString().toCharArray();
+                n--;
             }
-        }   
+        return String.valueOf(ch);
+    }
 }
-var stats="You took"+guess+"guesses to sink the battleship,"+"which means your shooting accuracy was"+(3/guesses);
-alert(stats);
