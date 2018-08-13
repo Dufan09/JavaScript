14c82400b59e231a49ad1d1ecc9896eb3fd7df16
Dufan09 <41882784+Dufan09@users.noreply.github.com>
2018-08-13 16:40:23 +0800

    "ComponentRegistration1.1

--- vuejs/ComponentRegistration1.js
+++ vuejs/ComponentRegistration1.js
@@ -1,11 +1,6 @@
 Vue.component('blog-post', {
     props: ['todo'],
-    template: 
-        `<ul class="item_header">
-            <div v-if='todo.dayline===true' class="dayline" >
-                <div class="line1"></div>
-                <span class="line_text">Yesterday</span>
-            </div>
+    template: `<ul class="item_header">
             <li>
                 <img v-bind:src="todo.H_picture" class="itype">
                 <span class="username">{{todo.username}}</span>
@@ -20,75 +15,73 @@ Vue.component('blog-post', {
                 </div><br>
                 <img v-bind:src="todo.r_picture" class="E_picture">
             </li>
-            <div  v-if='todo.cutline===true' class="line0">
-            </div>
         </ul>`
-    })
-new Vue({
-    el: '#blog-post-demo',
+})
+var info = new Vue({
+    el: '.dialog',
     data: {
-        todos:[
-      { H_picture:'image/1.png',
-        username:'Chirs,Peng',
-        action_a:'add',
-        theme:'Note',
-        mainpoint:'1/29/2018 Sprint Summary',
-        m_body:'Developers make sure that finish all new feature development and critical issue fixing and merge all code to master branch at this...',
-        time:'07:31PM',
-        r_picture:"image/7.png",
-        cutline:true,
-      },
-      { H_picture:'image/2.png',
-        username:'Chirs,Peng',
-        action_a:'updated',
-        theme:'Meeting',
-        mainpoint:'KKKKK',
-        m_body:'status on 2018/03/09 11:00 AM and assign to Outlook Ying,ting ma',
-        time:'07:31PM',
-        r_picture:"image/7.png",
-        cutline:true
-      },
-      { H_picture:'image/3.png',
-        username:'Chirs,Peng',
-        action_a:'sent',
-        theme:'Document',
-        mainpoint:'KKKKK.jpg',
-        body_picture: 'image/6.png',
-        time:'07:31PM',
-        r_picture:'image/7.png',
-      },
-      { H_picture:'image/4.png',
-        username: 'Chirs,Peng',
-        action_a: 'received an email to:',
-        emailfrom: 'allen@yhlsoft.com',
-        m_body: 'Re:Now US Release plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that mang...',
-        time: '07:31 PM',
-        r_picture:'image/8.png',
-        dayline:true,
-        cutline:true
-      }, 
-      { H_picture:'image/4.png',
-        username: 'Chirs,Peng',
-        action_a: 'received an email to:',
-        emailfrom: 'allen@yhlsoft.com',
-        m_body: 'Now US Release plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that mang...',
-        time: '07:31 PM',
-        cutline:true
-      }, 
-      { H_picture:'image/4.png',
-        username: 'Chirs,Peng',
-        action_a: 'received an email to:',
-        emailfrom: 'allen@yhlsoft.com',
-        m_body: 'Now US Release plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that mang...',
-        time: '07:31 PM',
-        cutline:true
-      },
-      { H_picture:'image/4.png',
-        username: 'Chirs,Peng',
-        action_a: 'received an email to:',
-        emailfrom: 'allen@yhlsoft.com',
-        m_body: 'Now US Release plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that mang...',
-        time: '07:31 PM'
-      }]
-    }
+        today: [{
+            H_picture: 'image/1.png',
+            username: 'Chirs,Peng',
+            action_a: 'add',
+            theme: 'Note',
+            mainpoint: '1/29/2018 Sprint Summary',
+            m_body: 'Developers make sure that finish all new feature development and critical issue fixing and merge all code to master branch at this...',
+            time: '07:31PM',
+            r_picture: "image/7.png",
+            cutline: true
+        }, {
+            H_picture: 'image/2.png',
+            username: 'Chirs,Peng',
+            action_a: 'updated',
+            theme: 'Meeting',
+            mainpoint: 'KKKKK',
+            m_body: 'status on 2018/03/09 11:00 AM and assign to Outlook Ying,ting ma',
+            time: '07:31PM',
+            r_picture: "image/7.png",
+            cutline: true
+        }, {
+            H_picture: 'image/3.png',
+            username: 'Chirs,Peng',
+            action_a: 'sent',
+            theme: 'Document',
+            mainpoint: 'KKKKK.jpg',
+            body_picture: 'image/6.png',
+            time: '07:31PM',
+            r_picture: 'image/7.png'
+        }],
+        yesterday:[ {
+            H_picture: 'image/4.png',
+            username: 'Chirs,Peng',
+            action_a: 'received:',
+            emailfrom: 'allen@yhlsoft.com',
+            m_body: 'Re:Now US Release plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that mang...',
+            time: '07:31 PM',
+            r_picture: 'image/8.png',
+            cutline: true
+        }, {
+            H_picture: 'image/4.png',
+            username: 'Chirs,Peng',
+            action_a: 'received an email to:',
+            emailfrom: 'allen@yhlsoft.com',
+            m_body: 'Now US Release plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that mang...',
+            time: '07:31 PM',
+            cutline: true
+        }, {
+            H_picture: 'image/4.png',
+            username: 'Chirs,Peng',
+            action_a: 'received an email to:',
+            emailfrom: 'allen@yhlsoft.com',
+            m_body: 'Now US Release plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that mang...',
+            time: '07:31 PM',
+            cutline: true
+        }, {
+            H_picture: 'image/4.png',
+            username: 'Chirs,Peng',
+            action_a: 'received an email to:',
+            emailfrom: 'allen@yhlsoft.com',
+            m_body: 'Now US Release plan-staging need support people to help testing Added new showstopper issuses for staging,the big issue is that mang...',
+            time: '07:31 PM'
+        }]
+    },
 })
